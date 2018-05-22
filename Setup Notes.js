b7317f67a740f7455ebc129a845a36ce1351e1f6
PolarisTLX <paul.a.rail@gmail.com>
2018-05-21 23:24:37 -0400

    preparing project

---
+++ Setup Notes.js
@@ -0,0 +1,13 @@
+/*
+PROJECT SETUP:
+
+Create and publish Github Repo on name. In this case: Facebook-Authorisation-React-App
+
+need to set up in the Command Prompt (CP):
+Gulp.js globally:  npm i -g gulp
+wepback globally:  npm i -g webpack
+turbo-cli  globally:   npm i -g turbo-cli
+
+Create the app in CP:  turbo new Facebook-Authorisation-React-App --react
+This puts the turbo.cli files into that Github Repo
+*/
